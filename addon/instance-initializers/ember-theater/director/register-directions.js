655e906679dce679ced7f56998e5d0f7cb5cf84d
null null null <null@ember.theater>
2016-05-28 09:44:36 -0700

    simplify direction injection

--- addon/instance-initializers/ember-theater/director/register-directions.js
+++ addon/instance-initializers/ember-theater/director/register-directions.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import { gatherTypes } from 'ember-theater';
+import { Script, ScriptProxy } from 'ember-theater-director';
 
 const { String: { camelize } } = Ember;
 
@@ -7,28 +8,19 @@ const applyName = function applyName(appInstance, camelizedName, dasherizedNamed
   const factory = appInstance.lookup(`ember-theater/director/direction:${dasherizedNamed}`);
 
   factory.reopen({
-    _name: camelizedName
+    directionName: camelizedName
   });
 };
 
-const injectIntoScript = function injectIntoScript(appInstance, camelizedName, dasherizedNamed) {
-  const directorProxy = function directorProxy(...args) {
+const injectIntoScript = function injectIntoScript(appInstance, camelizedName, dasherizedOrCamelizedNamed, ClassType) {
+  const directionProxy = { };
+
+  directionProxy[camelizedName] = function(...args) {
     // the scene is the context here
-    return this._executeDirection(dasherizedNamed, args);
+    return this._executeDirection(dasherizedOrCamelizedNamed, args);
   };
 
-  appInstance.register(`ember-theater/director/direction-proxy/script:${dasherizedNamed}`, directorProxy, { instantiate: false });
-  appInstance.inject('ember-theater/director:script', camelizedName, `ember-theater/director/direction-proxy/script:${dasherizedNamed}`);
-};
-
-const injectIntoScriptProxy = function injectIntoScriptProxy(appInstance, camelizedName, dasherizedNamed) {
-  const scriptProxy = function scriptProxy(...args) {
-    // the scene proxy is the context here
-    return this._executeDirection(camelizedName, args);
-  };
-
-  appInstance.register(`ember-theater/director/script-proxy/script:${dasherizedNamed}`, scriptProxy, { instantiate: false });
-  appInstance.inject('ember-theater/director:script-proxy', camelizedName, `ember-theater/director/script-proxy/script:${dasherizedNamed}`);
+  ClassType.reopen(directionProxy);
 };
 
 export function initialize(appInstance) {
@@ -38,8 +30,8 @@ export function initialize(appInstance) {
     const camelizedName = camelize(dasherizedNamed);
 
     applyName(appInstance, camelizedName, dasherizedNamed);
-    injectIntoScript(appInstance, camelizedName, dasherizedNamed);
-    injectIntoScriptProxy(appInstance, dasherizedNamed, camelizedName);
+    injectIntoScript(appInstance, camelizedName, dasherizedNamed, Script);
+    injectIntoScript(appInstance, camelizedName, camelizedName, ScriptProxy);
   });
 }
 
--- tests/dummy/app/ember-theater/director/directions/basic.js
+++ tests/dummy/app/ember-theater/director/directions/basic.js
@@ -15,7 +15,7 @@ export default Direction.extend({
     this._entryPoint();
 
     set(this, 'attrs.header', header);
-    set(this, 'attrs.appender', get(this, 'predecessors').findBy('_name', 'appender'));
+    set(this, 'attrs.appender', get(this, 'predecessors').findBy('directionName', 'appender'));
 
     return this;
   },
