2add45ad8d9682c3236163e1689b670f621f7466
null null null <null@revolution.cheap>
2016-07-05 13:32:23 -0700

    extract globals

--- addon/affinity-engine/configs/stage.js
+++ addon/affinity-engine/configs/stage.js
@@ -1,35 +1,5 @@
 export default {
   priority: 1,
-  globals: {
-    classNames: ['et-paper', 'et-block'],
-    transitionDuration: 200,
-    cps: 25,
-    tweenEffect: {
-      opacity: {
-        from: 0,
-        to: 1
-      }
-    },
-    tweenRate: 25,
-    transition: {
-      duration: 250,
-      effect: { opacity: 1 }
-    },
-    transitionIn: {
-      duration: 250,
-      effect: { opacity: [1, 0] }
-    },
-    transitionOut: {
-      duration: 250,
-      effect: { opacity: 0 }
-    },
-    keys: {
-      accept: ['Space', 'Enter'],
-      cancel: ['Escape'],
-      moveDown: ['ArrowDown', 's'],
-      moveUp: ['ArrowUp', 'w']
-    }
-  },
   stage: {
     scene: {
       transitionIn: {
